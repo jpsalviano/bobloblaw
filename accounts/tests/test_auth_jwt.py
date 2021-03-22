40d3c5ae2220b07e4991f454a499ecff204adc3d
João Paulo F. Salviano <jpfsalviano@gmail.com>
2021-03-22 11:22:34 -0300

    continuando endpoint auth-jwt

--- accounts/tests/test_auth_jwt.py
+++ accounts/tests/test_auth_jwt.py
@@ -1,3 +1,6 @@
+import jwt
+import json
+
 from django.test import TestCase, Client
 from django.urls import reverse
 from django.core.signing import Signer
@@ -15,6 +18,14 @@ class AuthJWT(TestCase):
     def test_auth_jwt_generates_token(self):
         payload = {"user_id": self.user.id}
         result = self.client.post(reverse("auth_jwt"), payload, content_type="application/json")
-        key1 = Signer().sign("JWT")
-        key2 = Signer().sign("JWT")
-        self.assertEqual(key1, key2)
+        secret_key = Signer().sign("JWT")
+        expected_result = json.dumps({"token": jwt.encode(payload, secret_key, "HS256")})
+        self.assertEqual(result.content.decode(), expected_result)
+
+    def test_auth_jwt_token_holds_correct_payload(self):
+        payload = {"user_id": self.user.id}
+        result = self.client.post(reverse("auth_jwt"), payload, content_type="application/json")
+        secret_key = Signer().sign("JWT")
+        result_token = result.json()["token"]
+        payload_from_result_token = jwt.decode(result_token, secret_key, algorithms=["HS256"])
+        self.assertEqual(payload_from_result_token, payload)
--- accounts/views/auth_jwt.py
+++ accounts/views/auth_jwt.py
@@ -10,5 +10,5 @@ class GenerateToken(View):
     def post(self, request):
         user_id = json.loads(request.body)["user_id"]
         secret_key = Signer().sign("JWT")
-        token = jwt.encode({"user_id": user_id}, secret_key, algorithm="HS256").decode("utf-8")
+        token = jwt.encode({"user_id": user_id}, secret_key, algorithm="HS256")
         return JsonResponse({"token": token})
