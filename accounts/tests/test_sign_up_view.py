63e8bc58a340e6d75830b3105933788327de955a
joao <jpfsalviano@gmail.com>
2021-03-24 11:32:16 -0300

    shortened rstr imports usage on sign in tests

--- accounts/tests/test_sign_up_view.py
+++ accounts/tests/test_sign_up_view.py
@@ -1,5 +1,5 @@
 import json
-import rstr
+from rstr import rstr, letters, nonwhitespace
 
 from django.test import TestCase, Client
 from django.db import models
@@ -15,8 +15,8 @@ class SignUp(TestCase):
 
     def test_sign_up_creates_user(self):
         payload = {
-                    "username": f"{rstr.rstr(rstr.letters(), 5, 50)}@{rstr.rstr(rstr.letters(), 4, 40)}.com",
-                    "password": "abc123-"
+                    "username": f"{rstr(letters(), 5, 50)}@{rstr(letters(), 4, 40)}.com",
+                    "password": f"{rstr(nonwhitespace(), 7)}"
                     }
         result = self.client.post(reverse('create_user'), payload, content_type="application/json")
         user = User.objects.filter(username=payload["username"])
@@ -26,8 +26,8 @@ class SignUp(TestCase):
 
     def test_sign_up_encrypts_password(self):
         payload = {
-                    "username": f"{rstr.rstr(rstr.letters(), 5, 50)}@{rstr.rstr(rstr.letters(), 4, 40)}.com",
-                    "password": f"{rstr.rstr(rstr.letters(), 10, 20)}-"
+                    "username": f"{rstr(letters(), 5, 50)}@{rstr(letters(), 4, 40)}.com",
+                    "password": f"{rstr(letters(), 10, 20)}-"
                     }
         self.client.post(reverse('create_user'), payload, content_type="application/json")
         stored_password = User.objects.get(username=payload["username"]).password
@@ -35,7 +35,7 @@ class SignUp(TestCase):
 
     def test_sign_up_responds_error_if_password_is_not_provided(self):
         payload = {
-                    "username": f"{rstr.rstr(rstr.letters(), 5, 50)}@{rstr.rstr(rstr.letters(), 4, 40)}.com"
+                    "username": f"{rstr(letters(), 5, 50)}@{rstr(letters(), 4, 40)}.com"
                     }
         expected_result = {'password': ['This field is required.']}
         result = self.client.post(reverse('create_user'), payload, content_type="application/json")
@@ -44,7 +44,7 @@ class SignUp(TestCase):
 
     def test_sign_up_responds_error_if_username_is_not_provided(self):
         payload = {
-                    "password": "abc123-"
+                    "password": f"{rstr(nonwhitespace(), 10, 20)}"
                     }
         expected_result = {'username': ['This field is required.']}
         result = self.client.post(reverse('create_user'), payload, content_type="application/json")
@@ -54,7 +54,7 @@ class SignUp(TestCase):
     def test_sign_up_responds_error_if_username_is_empty_string(self):
         payload = {
                     "username": "",
-                    "password": "abc123-"
+                    "password": f"{rstr(nonwhitespace(), 7)}"
                     }
         expected_result = {'username': ['This field is required.']}
         result = self.client.post(reverse('create_user'), payload, content_type="application/json")
@@ -63,8 +63,8 @@ class SignUp(TestCase):
 
     def test_sign_up_responds_error_if_password_is_too_short(self):
         payload = {
-                    "username": f"{rstr.rstr(rstr.letters(), 5, 50)}@{rstr.rstr(rstr.letters(), 4, 40)}.com",
-                    "password": "abc12-"
+                    "username": f"{rstr(letters(), 5, 50)}@{rstr(letters(), 4, 40)}.com",
+                    "password": f"{rstr(nonwhitespace(), 6)}"
                     }
         expected_result = {'password': ['Ensure this value has at least 7 characters (it has 6).']}
         result = self.client.post(reverse('create_user'), payload, content_type="application/json")
@@ -73,8 +73,8 @@ class SignUp(TestCase):
 
     def test_sign_up_responds_error_if_username_is_too_short(self):
         payload = {
-                    "username": "aa.com",
-                    "password": "abc123-"
+                    "username": f"{rstr(letters(), 4)}",
+                    "password": f"{rstr(nonwhitespace(), 7)}"
                     }
         expected_result = {'username': ['Enter a valid email address.']}
         result = self.client.post(reverse('create_user'), payload, content_type="application/json")
@@ -83,8 +83,8 @@ class SignUp(TestCase):
 
     def test_sign_up_responds_error_if_username_is_already_picked(self):
         payload = {
-                    "username": f"{rstr.rstr(rstr.letters(), 5, 50)}@{rstr.rstr(rstr.letters(), 4, 40)}.com",
-                    "password": "abc123-"
+                    "username": f"{rstr(letters(), 5, 50)}@{rstr(letters(), 4, 40)}.com",
+                    "password": f"{rstr(nonwhitespace(), 7)}"
                     }
         self.client.post(reverse('create_user'), payload, content_type="application/json")
         self.client.post(reverse('create_user'), payload, content_type="application/json")
